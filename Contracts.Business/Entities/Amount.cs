092843393dcb31e26438cd678e7bc33d45e7fd47
Andrey Kuzmenko <andrey.kuzmenko@yamalspg.ru>
2017-05-17 16:07:08 +0300

    CMAS-343 Мультивалютность договоров на backend'e

---
+++ Contracts.Business/Entities/Amount.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Cmas.BusinessLayers.Contracts.Entities
+{
+    /// <summary>
+    /// Стоимость договора
+    /// </summary>
+    public class Amount
+    {
+        // Валюта
+        public string CurrencySysName;
+
+        // значение
+        public double Value = 0;
+    }
+}
--- Contracts.Business/Entities/Contract.cs
+++ Contracts.Business/Entities/Contract.cs
@@ -1,37 +1,79 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Cmas.BusinessLayers.Contracts.Entities
 {
     public class Contract
     {
+        /// <summary>
+        /// Внутренний идентификатор
+        /// </summary>
         public string Id;
 
+        /// <summary>
+        /// Дата обновления
+        /// </summary>
         public DateTime UpdatedAt;
 
+        /// <summary>
+        /// Дата создания
+        /// </summary>
         public DateTime CreatedAt;
-        
+
+        /// <summary>
+        /// Название договора
+        /// </summary>
         public string Name;
 
+        /// <summary>
+        /// Номер договора
+        /// </summary>
         public string Number;
 
+        /// <summary>
+        /// Дата заключения
+        /// </summary>
         public DateTime? StartDate;
 
+        /// <summary>
+        /// Дата окончания
+        /// </summary>
         public DateTime? FinishDate;
 
+        /// <summary>
+        /// Имя подрядчика
+        /// </summary>
         public string ContractorName;
 
-        public string Currency;
+        /// <summary>
+        /// Стоимости договора
+        /// </summary>
+        public IList<Amount> Amounts;
 
-        public double Amount;
-
+        /// <summary>
+        /// НДC включен в стоимость договора
+        /// </summary>
         public bool VatIncluded;
 
+        /// <summary>
+        /// Название объекта строительства
+        /// </summary>
         public string ConstructionObjectName;
 
+        /// <summary>
+        /// Название объекта строительства по титульному списку
+        /// </summary>
         public string ConstructionObjectTitleName;
 
+        /// <summary>
+        /// Код титульного списка объекта строительства
+        /// </summary>
         public string ConstructionObjectTitleCode;
 
+        /// <summary>
+        /// Примечания к договору
+        /// </summary>
         public string Description;
 
         /// <summary>
